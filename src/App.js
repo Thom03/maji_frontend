73b509f6a1b919ec88b96b123e522bb3b34ffd90
thom03 <Dr3@mchaser>
2021-09-02 23:11:45 +0300

    Added the base admin theme.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import Content from './components/Content';
 import Nav from "./components/Nav";
 import SideMenu from "./components/SideMenu";
 
 
 function App() {
   return (
-      <div>
-          <Nav/>
+      <div id="wrapper">
           <SideMenu/>
+          <Content/>
+         
       </div>
   );
 }
